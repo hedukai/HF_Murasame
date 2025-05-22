88eb39e008ed24f655998bbf08d1319899a9931b
hedukai <961285867@qq.com>
2025-05-22 21:13:04 +0800

    初次提交

--- src/main/java/com/hdk/high_frequency_murasame/item/hf_murasama.java
+++ src/main/java/com/hdk/high_frequency_murasame/item/hf_murasama.java
@@ -14,24 +14,21 @@ public class hf_murasama extends SwordItem{
     public hf_murasama(Tier tier, int attackDamage, float attackSpeed, Properties properties) {
         super(tier, attackDamage, attackSpeed, properties);
     }
-    // 无限耐久：物品不会被消耗
     @Override
     public boolean isDamageable(ItemStack stack) {
-        return false; // 禁止耐久损耗
+        return false;
     }
     @Override
     public boolean isEnchantable(@NotNull ItemStack stack) {
         return true;
     }
-    // 控制可接受的附魔类型
     @Override
     public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
         return enchantment.category == EnchantmentCategory.WEAPON;
     }
-    // 设置附魔强度（影响附魔等级）
     @Override
     public int getEnchantmentValue() {
-        return 10; // 原版钻石剑为 10
+        return 10;
     }
 
 
